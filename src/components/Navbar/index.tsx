843cae5dc0f3cf691f16bc28cdd7ca8e116050c2
roidsaja <roid0520@gmail.com>
2022-08-17 20:34:02 +0700

    Style: changed the navbar to include logo icon

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -3,23 +3,29 @@ import styled from "styled-components";
 import Image from "next/image";
 import { useRouter } from "next/router";
 
-const navigationRoutes = ["home", "about", "operators"];
+const navigationRoutes = ["about", "operators"];
 
 const Navbar: React.FC = () => {
   const router = useRouter();
   return (
     <NavbarWrapper>
-      {navigationRoutes.map((singleRoute) => {
-        return (
-          <NavigationLink
-            key={singleRoute}
-            href={`/${singleRoute}`}
-            text={singleRoute}
-            router={router}
-            className="container"
-          />
-        );
-      })}
+      <div className="nav_logo">
+        <Link href="/">
+          <Image src="/Arknights-logo.jpeg" width="70" height="70" />
+        </Link>
+      </div>
+      <div className="nav_menu">
+        {navigationRoutes.map((singleRoute) => {
+          return (
+            <NavigationLink
+              key={singleRoute}
+              href={`/${singleRoute}`}
+              text={singleRoute}
+              router={router}
+            />
+          );
+        })}
+      </div>
     </NavbarWrapper>
   );
 };
@@ -40,26 +46,30 @@ const NavigationLink = ({ href, text, router }: any) => {
 
 const NavbarWrapper = styled.nav`
   display: flex;
-  justify-content: space-between;
   padding-top 1rem;
+  justify-content: space-between;
   padding-bottom: 0.75rem;
   padding-left: 1rem;
   padding-right: 1rem;
   box-shadow: 0 20px 25px -5px rgb(0 0 0 / 0.1), 0 10px 10px -6px rgb(0 0 0 / 0.1);
   border-style: hidden;
 
-  .container {
-    justify-content: space-between;
-    margin: 0 auto;
-    padding-right: 1rem;
-    padding-left: 1rem;
+  .nav_logo {
+    display: flex;
+    align-items: center;
+  }
+
+  .nav_menu {
+    display: flex;
+    align-items: center;
   }
 
   .nav_item {
     text-transform: capitalize;
 	color: gray;
 	text-decoration: none;
-	padding: 5px 10px;
+	padding: 0 10px;
+    margin: 0 10px;
 	border-bottom: 2px solid transparent;
   }
 
